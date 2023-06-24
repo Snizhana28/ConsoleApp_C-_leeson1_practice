a18df7eb0377a357ddc8fb2dbde52e10c1f70ab4
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-06-24 17:33:43 +0300

    Done task 6

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ConsoleApp_C__leeson1_practice
+﻿using System.Text;
+
+namespace ConsoleApp_C__leeson1_practice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            /*
+             Користувач з клавіатури вводить довжину лінії, символ 
+             заповнювач, напрямок лінії (горизонтальна, вертикальна). 
+             Програма відображає лінію по заданих параметрах. 
+             Наприклад: +++++.
+             Параметри лінії: горизонтальна лінія, довжина дорівнює п'яти, символ заповнювач +.
+             */
+
+
         }
     }
 }
