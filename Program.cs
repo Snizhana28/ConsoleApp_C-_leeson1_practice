0cc6641abbf0029c3600df77286cff82dbe28e70
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-06-24 17:23:12 +0300

    Done task 5

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            int A = 3;
+            int B = 7;
+            for (int i = A; i <= B; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
