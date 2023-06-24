25557de2960d618cdd3b7acc80ed80d6035807c0
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-06-24 16:58:20 +0300

    Done task 2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            
+            int[] arr = new int[5];
+            int sum = 0;
+            int max = 0;
+            int min = 1;
+            int mult = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter number: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum += arr[i];
+                mult *= arr[i];
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("Sum of numbers: " + sum);
+            Console.WriteLine("Max number: " + max);
+            Console.WriteLine("Min number: " + min);
+            Console.WriteLine("Multiplication of numbers: " + mult);
+            Console.ReadKey();
         }
     }
 }
