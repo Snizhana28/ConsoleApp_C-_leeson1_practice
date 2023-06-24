86612344c40e9dba7b0170cadd1076b1f7875156
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-06-24 17:44:19 +0300

    Done task 6

--- Program.cs
+++ Program.cs
@@ -8,15 +8,31 @@ namespace ConsoleApp_C__leeson1_practice
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
-            /*
-             Користувач з клавіатури вводить довжину лінії, символ 
-             заповнювач, напрямок лінії (горизонтальна, вертикальна). 
-             Програма відображає лінію по заданих параметрах. 
-             Наприклад: +++++.
-             Параметри лінії: горизонтальна лінія, довжина дорівнює п'яти, символ заповнювач +.
-             */
-
-
+            Console.WriteLine("Введіть довжину лінії");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть символ заповнювач");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введіть напрямок лінії (горизонтальна - x , вертикальна - y )");
+            string direction = Console.ReadLine();
+            if (direction == "x")
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write(symbol);
+                }
+            }
+            else if (direction == "y")
+            {
+               for (int i = 0; i < length; i++)
+                {
+                    Console.WriteLine(symbol);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Невірно введені дані");
+            }
+            Console.ReadKey();
         }
     }
 }
